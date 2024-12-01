e53fd4c50a1422aaba55b921a94e5da69283a83e
etabebe1 <etabebe1@gmail.com>
2024-12-01 04:20:36 -0500

    updated StatusNFT.sol

--- src/StatusNFT.sol
+++ src/StatusNFT.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.26;
 
-import {ERC721} from "@openzeppelin/"
+import {ERC721} from "@openzeppelin/token/ERC721/ERC721.sol";
 
 contract StatusNFT is ERC721 {
     uint256 public s_tokenCounter;
     string public s_unverified;
     string public s_verified;
 
-    constructor(string memory _unverified, string memory _verified) ERC721("User Status","US") {
+    constructor(string memory _unverified, string memory _verified) ERC721("User Status", "US") {
         s_tokenCounter = 0;
         s_unverified = _unverified;
         s_verified = _verified;
